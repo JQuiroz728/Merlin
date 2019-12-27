2b8778b15c8acf6dc0000ae9e7412a1330fd14a2
Jorge Quiroz <gerschjr@gmail.com>
2019-12-26 23:49:38 -0500

    update

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ import random
 from gtts import gTTS
 from time import ctime
 
+"""
+Voice Assistant application that uses Google's speech regonition library 
+and text-to-speech API to process speech inputs and communicate with the user
+
+Currently supports: 
+ - Getting current time and date
+ - Obtaining search results via Google
+ - Getting directions for a given location via Google Maps
+"""
+
 r = sr.Recognizer()
 
 def captureAudio(ask = False):
@@ -24,6 +34,7 @@ def captureAudio(ask = False):
         return voiceData
 
 def merlinSpeak(audioString):
+    # Communicates through created audio files that are played to respond to user and are then removed from memory
     tts = gTTS(text = audioString, lang = 'en')
     r = random.randint(1, 10000000)
     audioFile = 'audio-' + str(r) + '.mp3'
@@ -33,6 +44,7 @@ def merlinSpeak(audioString):
     os.remove(audioFile)
 
 def respond(voiceData):
+    # Currently only supports these few functionalities through specific word matching
     if 'what is your name' in voiceData:
         merlinSpeak('Hello my name is Merlin')
     if 'what time is it' in voiceData:
@@ -51,7 +63,7 @@ def respond(voiceData):
         exit()
 
 time.sleep(1)
-merlinSpeak('My name is Merlin. How can I help?')
+merlinSpeak('My name is Merlin. How can I help?') # Introduction
 while 1:
     voiceData = captureAudio()
     respond(voiceData)
